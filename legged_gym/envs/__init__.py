8eb55caa53e4de6a04df09308931f8f9d3c27267
caoliu <l-cao20@mails.tsinghua.edu.cn>
2023-11-21 10:51:21 +0000

    rsl_rl-1.0.2 legged_gym environment worked well with PPO

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -38,6 +38,7 @@ from .anymal_b.anymal_b_config import AnymalBRoughCfg, AnymalBRoughCfgPPO
 from .cassie.cassie import Cassie
 from .cassie.cassie_config import CassieRoughCfg, CassieRoughCfgPPO
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
+from .go1.go1_config import Go1RoughCfg, Go1RoughCfgPPO
 
 
 import os
@@ -47,5 +48,6 @@ from legged_gym.utils.task_registry import task_registry
 task_registry.register( "anymal_c_rough", Anymal, AnymalCRoughCfg(), AnymalCRoughCfgPPO() )
 task_registry.register( "anymal_c_flat", Anymal, AnymalCFlatCfg(), AnymalCFlatCfgPPO() )
 task_registry.register( "anymal_b", Anymal, AnymalBRoughCfg(), AnymalBRoughCfgPPO() )
-task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
 task_registry.register( "cassie", Cassie, CassieRoughCfg(), CassieRoughCfgPPO() )
+task_registry.register( "a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO() )
+task_registry.register( "go1", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO() )
